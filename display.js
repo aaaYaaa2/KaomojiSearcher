6dcaba508214b4da236a7df32b6ee971a0415fdf
Yuuki221 <xcicada824@gmail.com>
2016-06-19 15:44:08 -0400

    add copy and paste feature

--- display.js
+++ display.js
@@ -3,7 +3,8 @@ $(window).on('load', function(){
 		var container = $('#kaomoji-container');
 		Object.keys(kaomojis).forEach(function(key){
 			var kaomoji = kaomojis[key];
-			var label = kaomoji['icon'] + '&nbsp;&nbsp;&nbsp;' + key + '<span class="keywords">' + key + ' ' + kaomoji['keywords'] + '</span>';
+			var label = '<input type="text" class="kaomoji-icon" value= "'+kaomoji['icon']+'" />' + 
+			'<br />' + key + '<span class="keywords">' + key + ' ' + kaomoji['keywords'] + '</span>';
 			container.append('<li class="possible_result">' + label + '</li>'); 
 			// set the id of each li to the key of the kaomoji 
 		});
@@ -14,3 +15,21 @@ $('#search').on('search keyup',function(){
 	window.location.hash = $(this).val().trim().split(' ').join('_');
 	// display the search value behind the hash table 
 });
+
+// naive copy and paste of javascript 
+$(document).on('click', '.kaomoji-icon',function(){
+	//if(document.queryCommandSupport('copy')){
+		this.select();
+		// Selection and execCommand does not support in Safari 
+		if(document.queryCommandSupported("copy")){
+			document.execCommand('copy');  // copyed!
+			window.getSelection().removeAllRanges(); // leave the anchorNode focusNode properties to null
+			alert(this.value + " Copied! ");
+		}else{
+			alert("Please use Control+C to copy.");
+		}
+});
+
+// 
+
+
